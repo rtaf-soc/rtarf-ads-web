a94733dfe27cba83427b4363da482b2c1224c853
spinater <spinater0@gmail.com>
2025-05-19 12:17:24 +0700

    fix device uptime page

--- pages/numa_status.vue
+++ pages/numa_status.vue
@@ -4,12 +4,16 @@
       <!-- <q-card-section class="q-pb-none">
         <h5 class="q-pa-none q-ma-none">สถานะอุปกรณ์เครือข่าย</h5>
       </q-card-section> -->
-      <q-card-section>
-        <iframe :src="machineStatusURL" width="100%" height="100%" style="border: none;"></iframe>
+      <q-card-section class="q-pa-none q-ma-none" style="height: 100vh;">
+        <iframe sandbox="allow-scripts allow-same-origin" :src="machineStatusURL" width="100%" height="100%"
+          style="border: none;"></iframe>
 
       </q-card-section>
     </q-card>
-
+    <!-- <q-card-section class="q-pa-none q-ma-none" style="height: 100vh;">
+        <iframe sandbox="allow-scripts allow-same-origin" :src="url03" width="100%" height="100%"
+          style="border: none;"></iframe>
+      </q-card-section> -->
   </q-page>
 </template>
 
